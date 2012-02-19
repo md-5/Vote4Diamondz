0a7d6e1bbc2c391f24d1a42a386829ec56ead5f1
md_5 <md_5@bigpond.com>
2012-02-19 15:08:57 +1100

    Ready for new release, all in one class file too

--- src/main/java/net/shadowraze/vote4diamondz/Vote4Diamondz.java
+++ src/main/java/net/shadowraze/vote4diamondz/Vote4Diamondz.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,7 +22,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Vote4Diamondz extends JavaPlugin {
 
     private WebServer server;
-    public static ItemStack reward;
+    private ItemStack reward;
+    private final String URL = "jdbc:sqlite:plugins/Vote4Diamondz/users.sqlite";
 
     @Override
     public void onEnable() {
@@ -29,7 +31,7 @@ public class Vote4Diamondz extends JavaPlugin {
         conf.options().copyDefaults(true);
         saveConfig();
         reward = new ItemStack(conf.getInt("id"), conf.getInt("amount"));
-        Database.init();
+        init();
         server = new WebServer(conf.getInt("port"));
         server.start();
     }
@@ -41,38 +43,51 @@ public class Vote4Diamondz extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        ArrayList<String> top = Database.loadTop();
+        ArrayList<String> top = loadTop(3);
         if (top.size() < 3) {
             sender.sendMessage(ChatColor.RED + "Vote4Diamondz: Error less than 3 people have voted");
             return true;
         }
         sender.sendMessage(ChatColor.BLUE + "Vote4Diamondz by md_5 top voters: ");
-        sender.sendMessage(ChatColor.BLUE + "1) " + top.get(0));
-        sender.sendMessage(ChatColor.BLUE + "2) " + top.get(1));
-        sender.sendMessage(ChatColor.BLUE + "3) " + top.get(2));
+        for (int i = 0; i < top.size(); i++) {
+            sender.sendMessage(ChatColor.BLUE.toString() + (i + 1) + ") " + top.get(i));
+        }
         return true;
     }
 
-    public static int currentTime() {
+    private void init() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(URL);
+            Statement stat = conn.createStatement();
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS players (name text, time numeric, count numeric)");
+            stat.close();
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private int currentTime() {
         return (int) (System.currentTimeMillis() / 1000L);
     }
 
-    public static void processInput(String name) {
+    private void processInput(String name) {
         // Process
-        HashMap<String, Integer> query = Database.load(name);
+        HashMap<String, Integer> query = load(name);
         // Add new users
         if (query.isEmpty()) {
-            Database.add(name);
+            add(name);
         }
-        query = Database.load(name);
+        query = load(name);
         int time = query.get("time");
         int count = query.get("count");
         Player player = Bukkit.getServer().getPlayer(name);
         if (player != null) {
             if (currentTime() - time >= 86400) {
                 player.sendMessage(ChatColor.LIGHT_PURPLE + "You have received your reward. Thanks for voting!");
-                player.getInventory().addItem(Vote4Diamondz.reward);
-                Database.update(name, currentTime(), count + 1);
+                player.getInventory().addItem(reward);
+                update(name, currentTime(), count + 1);
             } else {
                 player.sendMessage(ChatColor.RED + "You can only vote once per day!");
             }
@@ -127,18 +142,90 @@ public class Vote4Diamondz extends JavaPlugin {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
                 String input = in.readLine();
-                String query = input.substring(5, input.length() - 9);
-                Vote4Diamondz.processInput(query.toLowerCase());
+                String query = input.substring(5, input.length() - 9).toLowerCase();
                 out.write(("HTTP/1.1 200 OK\r\n"
                         + "Date: " + new Date().toString() + "\r\n"
                         + "Content-Type: text/html\r\n"
                         + "\r\n"
                         + "\r\n").getBytes());
+                if (!query.isEmpty()) {
+                    processInput(query);
+                } else {
+                    for (String name : loadTop(0)) {
+                        out.write((name + " has voted " + load(name).get("count") + " times <br>\n").getBytes());
+                    }
+                }
                 out.flush();
                 out.close();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
+    }
+
+    private void add(String player) {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            PreparedStatement stat = conn.prepareStatement("INSERT INTO players VALUES (?,0,0)");
+            stat.setString(1, player);
+            stat.executeUpdate();
+            stat.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private HashMap<String, Integer> load(String player) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            PreparedStatement stat = conn.prepareStatement("SELECT time,count FROM players WHERE name = ?");
+            stat.setString(1, player);
+            ResultSet rs = stat.executeQuery();
+            while (rs.next()) {
+                result.put("time", rs.getInt("time"));
+                result.put("count", rs.getInt("count"));
+            }
+            stat.close();
+            rs.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    private void update(String name, int time, int count) {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            PreparedStatement stat = conn.prepareStatement("UPDATE players SET time = ?, count = ? WHERE name = ?");
+            stat.setInt(1, time);
+            stat.setInt(2, count);
+            stat.setString(3, name);
+            stat.executeUpdate();
+            stat.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private ArrayList<String> loadTop(int limit) {
+        ArrayList<String> top = new ArrayList<String>();
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT name FROM players ORDER BY count DESC" + ((limit <= 0) ? "" : " LIMIT " + limit));
+            while (rs.next()) {
+                top.add(rs.getString("name"));
+            }
+            stat.close();
+            rs.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return top;
     }
 }
