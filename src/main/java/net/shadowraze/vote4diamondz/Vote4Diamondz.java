bb5aac1f3f8a4092ec495580ec64d55d7e4fc9c4
md_5 <md_5@bigpond.com>
2012-09-16 18:47:08 +1000

    Initial v2.0 commit

--- src/main/java/net/shadowraze/vote4diamondz/Vote4Diamondz.java
+++ src/main/java/net/shadowraze/vote4diamondz/Vote4Diamondz.java
@@ -1,305 +1,45 @@
 package net.shadowraze.vote4diamondz;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.sql.*;
-import java.text.DateFormat;
-import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Vote4Diamondz extends JavaPlugin {
 
-    private WebServer server;
-    private List<String> commands;
-    private String message;
-    private String nag;
-    private String header;
-    private int nagTime;
-    private boolean broadcast;
-    public static final int INTERVAL = 86400;
-    private static final String URL = "jdbc:sqlite:plugins/Vote4Diamondz/users.sqlite";
-    private static final Logger logger = Logger.getLogger("Vote4Diamondz");
-    private static final DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private WebServer voteServer;
 
     @Override
     public void onEnable() {
+        // load the config
         FileConfiguration conf = getConfig();
         conf.options().copyDefaults(true);
         saveConfig();
-        //
-        logger.setUseParentHandlers(false);
+        // start the server
         try {
-            File logFile = new File(getDataFolder(), "votes.log");
-            logFile.createNewFile();
-            Handler handler = new FileHandler(logFile.getPath(), true);
-            handler.setFormatter(new Formatter() {
-                @Override
-                public String format(LogRecord record) {
-                    return "[" + format.format(System.currentTimeMillis()) + "] " + record.getMessage() + "\n";
-                }
-            });
-            logger.addHandler(handler);
+            voteServer = new VoteServer(new InetSocketAddress(conf.getString("host"), conf.getInt("port")));
         } catch (IOException ex) {
+            getLogger().severe("Could not start vote server: ");
             ex.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
         }
-        //
-        commands = conf.getStringList("commands");
-        broadcast = conf.getBoolean("broadcast");
-        message = ChatColor.translateAlternateColorCodes('&', conf.getString("message"));
-        nag = ChatColor.translateAlternateColorCodes('&', conf.getString("nag"));
-        nagTime = conf.getInt("nag-time");
-        if (nagTime < 5) {
-            throw new IllegalArgumentException("Cannot set nag interval lower than 5 minutes");
-        }
-        header = conf.getString("header");
-        //
-        init();
-        //
-        server = new WebServer(conf.getInt("port"));
-        server.start();
-        //
-        getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
-            public void run() {
-                final List<Player> toNag = new ArrayList<Player>();
-                for (Player player : getServer().getOnlinePlayers()) {
-                    HashMap<String, Integer> query = load(player.getName());
-                    int time = query.get("time");
-                    if (currentTime() - time >= INTERVAL || time == 0) {
-                        toNag.add(player);
-                    }
-                }
-                getServer().getScheduler().callSyncMethod(Vote4Diamondz.this, new Callable() {
-                    public Object call() throws Exception {
-                        for (Player player : toNag) {
-                            player.sendMessage(MessageFormat.format(nag, player.getName()));
-                        }
-                        return null;
-                    }
-                });
-            }
-        }, nagTime * 1200, nagTime * 1200);
     }
 
     @Override
     public void onDisable() {
-        server.shutdown();
+        super.onDisable();
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        ArrayList<String> top = loadTop(3);
-        if (top.size() < 3) {
-            sender.sendMessage(ChatColor.RED + "Vote4Diamondz: Error less than 3 people have voted");
-            return true;
-        }
-        sender.sendMessage(ChatColor.BLUE + "Vote4Diamondz by md_5 top voters: ");
-        for (int i = 0; i < top.size(); i++) {
-            sender.sendMessage(ChatColor.BLUE.toString() + (i + 1) + ") " + top.get(i));
-        }
-        return true;
-    }
+    private class VoteServer extends WebServer {
 
-    private void init() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection(URL);
-            Statement stat = conn.createStatement();
-            stat.executeUpdate("CREATE TABLE IF NOT EXISTS players (name text, time numeric, count numeric)");
-            stat.close();
-            conn.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private int currentTime() {
-        return (int) (System.currentTimeMillis() / 1000L);
-    }
-
-    private void processInput(String name) {
-        // Process
-        Player player = Bukkit.getServer().getPlayerExact(name);
-        if (player != null) {
-            HashMap<String, Integer> query = load(name);
-            int time = query.get("time");
-            int count = query.get("count");
-            if (currentTime() - time >= INTERVAL) {
-                if (broadcast) {
-                    getServer().broadcastMessage(MessageFormat.format(message, name));
-                } else {
-                    player.sendMessage(ChatColor.LIGHT_PURPLE + "You have received your reward. Thanks for voting!");
-                }
-                for (String command : commands) {
-                    getServer().dispatchCommand(getServer().getConsoleSender(), MessageFormat.format(command, name));
-                }
-                update(name, currentTime(), count + 1);
-            } else {
-                player.sendMessage(ChatColor.RED + "You can only vote once per day!");
-            }
-        }
-    }
-
-    private class WebServer extends Thread {
-
-        public ServerSocket serverSocket;
-        public boolean running = true;
-
-        public WebServer(int port) {
-            try {
-                serverSocket = new ServerSocket(port);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+        public VoteServer(InetSocketAddress address) throws IOException {
+            super(address);
         }
 
         @Override
-        public void run() {
-            while (running) {
-                try {
-                    new RequestThread(serverSocket.accept()).start();
-                } catch (IOException ex) {
-                }
-            }
+        protected String handle(SocketChannel client, String request) {
+            throw new UnsupportedOperationException("Not supported yet.");
         }
-
-        public void shutdown() {
-            try {
-                running = false;
-                serverSocket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    private class RequestThread extends Thread {
-
-        private Socket socket;
-
-        public RequestThread(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-                String input = in.readLine();
-                String query = input.substring(5, input.length() - 9).toLowerCase();
-                out.write(("HTTP/1.1 200 OK\r\n"
-                        + "Date: " + new Date().toString() + "\r\n"
-                        + "Content-Type: text/html\r\n"
-                        + "\r\n").getBytes());
-                if (!query.isEmpty() && !query.equals("favicon.ico")) {
-                    logger.info("Got request from: " + socket.getInetAddress() + " user was: " + query);
-                    processInput(query);
-                } else {
-                    out.write(header.getBytes());
-                    for (String name : loadTop(0)) {
-                        int count = load(name).get("count");
-                        if (count != 0) {
-                            out.write((name + " has voted " + count + " times <br>\n").getBytes());
-                        }
-                    }
-                }
-                out.flush();
-                socket.close();
-            } catch (Exception ex) {
-                //ex.printStackTrace();
-            }
-        }
-    }
-
-    private HashMap<String, Integer> add(String player) {
-        try {
-            Connection conn = DriverManager.getConnection(URL);
-            PreparedStatement stat = conn.prepareStatement("INSERT INTO players VALUES (?,0,0)");
-            stat.setString(1, player);
-            stat.executeUpdate();
-            stat.close();
-            conn.close();
-            return load(player);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
-    private HashMap<String, Integer> load(String player) {
-        HashMap<String, Integer> result = new HashMap<String, Integer>();
-        try {
-            Connection conn = DriverManager.getConnection(URL);
-            PreparedStatement stat = conn.prepareStatement("SELECT time,count FROM players WHERE name = ?");
-            stat.setString(1, player);
-            ResultSet rs = stat.executeQuery();
-            if (rs.next()) {
-                result.put("time", rs.getInt("time"));
-                result.put("count", rs.getInt("count"));
-            } else {
-                result = add(player);
-            }
-            stat.close();
-            rs.close();
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return result;
-    }
-
-    private void update(String name, int time, int count) {
-        try {
-            Connection conn = DriverManager.getConnection(URL);
-            PreparedStatement stat = conn.prepareStatement("UPDATE players SET time = ?, count = ? WHERE name = ?");
-            stat.setInt(1, time);
-            stat.setInt(2, count);
-            stat.setString(3, name);
-            stat.executeUpdate();
-            stat.close();
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private ArrayList<String> loadTop(int limit) {
-        ArrayList<String> top = new ArrayList<String>();
-        try {
-            Connection conn = DriverManager.getConnection(URL);
-            Statement stat = conn.createStatement();
-            ResultSet rs = stat.executeQuery("SELECT name FROM players ORDER BY count DESC" + ((limit <= 0) ? "" : " LIMIT " + limit));
-            while (rs.next()) {
-                top.add(rs.getString("name"));
-            }
-            stat.close();
-            rs.close();
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return top;
     }
 }
---
+++ src/main/java/net/shadowraze/vote4diamondz/WebServer.java
@@ -0,0 +1,134 @@
+package net.shadowraze.vote4diamondz;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Iterator;
+
+/**
+ * Simple Java non-blocking NIO webserver.
+ *
+ * @author md_5
+ */
+public abstract class WebServer implements Runnable {
+
+    private ByteBuffer buf = ByteBuffer.allocate(2048);
+    private Charset charset = Charset.forName("UTF-8");
+    private CharsetDecoder decoder = charset.newDecoder();
+    private CharsetEncoder encoder = charset.newEncoder();
+    private Selector selector = Selector.open();
+    private ServerSocketChannel server = ServerSocketChannel.open();
+    private boolean isRunning = true;
+
+    /**
+     * Create a new server and immediately binds it.
+     *
+     * @param address the address to bind on
+     * @throws IOException if there are any errors creating the server.
+     */
+    protected WebServer(InetSocketAddress address) throws IOException {
+        server.socket().bind(address);
+        server.configureBlocking(false);
+        server.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    /**
+     * Core run method. This is not a thread safe method, however it is non
+     * blocking. If an exception is encountered it will be thrown wrapped in a
+     * RuntimeException, and the server will automatically be {@link #shutDown}
+     */
+    @Override
+    public final void run() {
+        if (isRunning) {
+            try {
+                selector.selectNow();
+                Iterator<SelectionKey> i = selector.selectedKeys().iterator();
+                while (i.hasNext()) {
+                    SelectionKey key = i.next();
+                    i.remove();
+                    if (!key.isValid()) {
+                        continue;
+                    }
+                    try {
+                        // get a new connection
+                        if (key.isAcceptable()) {
+                            // accept them
+                            SocketChannel client = server.accept();
+                            // non blocking please
+                            client.configureBlocking(false);
+                            // show out intentions
+                            client.register(selector, SelectionKey.OP_READ);
+                            // read from the connection
+                        } else if (key.isReadable()) {
+                            //  get the client
+                            SocketChannel client = (SocketChannel) key.channel();
+                            // reset our buffer
+                            buf.rewind();
+                            // read into it
+                            client.read(buf);
+                            // flip it so we can decode it
+                            buf.flip();
+                            // decode the bytes, handle it, and write the response
+                            client.write(encoder.encode(CharBuffer.wrap("HTTP/1.1 200 OK\r\n\r\n" + handle(client, decoder.decode(buf).toString()) + "\r\n")));
+                        }
+                    } catch (Exception ex) {
+                        System.err.println("Error handling client: " + key.channel());
+                        System.err.println(ex);
+                        System.err.println("\tat " + ex.getStackTrace()[0]);
+                    } finally {
+                        if (key.channel() instanceof SocketChannel) {
+                            try {
+                                key.channel().close();
+                            } catch (IOException ex) {
+                                // too late to do anything here
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ex) {
+                // throw it as a runtime exception so that Bukkit can handle it
+                throw new RuntimeException(ex);
+            } finally {
+                // we are toast, lets not execute again
+                shutdown();
+            }
+        }
+    }
+
+    /**
+     * Handle a web request.
+     *
+     * @param client
+     * @param request entire request as sent by the client.
+     * @return the string to be sent to the client. Must not include HTTP
+     * headers.
+     */
+    protected abstract String handle(SocketChannel client, String request);
+
+    /**
+     * Shutdown this server, preventing it from handling any more requests.
+     */
+    public void shutdown() {
+        isRunning = false;
+        try {
+            selector.close();
+            server.close();
+        } catch (IOException ex) {
+            // do nothing, its game over
+        }
+        buf = null;
+        charset = null;
+        decoder = null;
+        encoder = null;
+        selector = null;
+        server = null;
+    }
+}
